185ddd5c149f798b0684a7a1b5d1d19dd735789c
Code Faisal <code.faisal.dev@gmail.com>
2024-07-08 20:47:18 +0600

    3ed commit

--- app/Components/Portfolio.jsx
+++ app/Components/Portfolio.jsx
@@ -8,7 +8,7 @@ const Portfolio = () => {
       <h1 className="text-center max-md:text-4xl max-md:mt-16 mb-16 text-6xl font-bold">
         My Works
       </h1>
-      <section className="grid items-center justify-center  grid-rows-2 xl:grid-cols-2 ">
+      <section className="grid items-center justify-center gap-5 grid-rows-2 xl:grid-cols-2 ">
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
